f5cc9dbfa86848423b59afd8436d59963a343d83
Riccardo-Gucciardo <riccardogucciardo003@gmail.com>
2025-02-12 22:22:59 +0100

    modifiche1

--- controllers/postController.js
+++ controllers/postController.js
@@ -8,27 +8,27 @@ const arrayPosts = require("../data/posts.js")
 
 // index
 router.get('/', function (req, res) {
-    res.send('Lista delle pizze');
+    res.send('Lista dei post');
     });
     // show
     router.get('/:id', function (req, res) {
-    res.send(`Dettagli della pizza ${req.params.id}`);
+    res.send(`Dettagli dei post ${req.params.id}`);
     });
     // store
     router.post('/', function (req, res) {
-    res.send('Creazione nuova pizza');
+    res.send('Creazione nuovo post');
     });
     // update
     router.put('/:id', function (req, res) {
-    res.send(`Modifica integrale della pizza ${req.params.id} `);
+    res.send(`Modifica integrale del post ${req.params.id} `);
     });
     // modify
     router.patch('/:id', function (req, res) {
-    res.send(`Modifica parziale della pizza ${req.params.id} `);
+    res.send(`Modifica parziale del post ${req.params.id} `);
     });
     // destroy
     router.delete('/:id', function (req, res) {
-    res.send(`Eliminazione della pizza  ${req.params.id}`);
+    res.send(`Eliminazione del post  ${req.params.id}`);
     });
 
 
