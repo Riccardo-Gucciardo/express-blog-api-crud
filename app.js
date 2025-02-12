adce9ec18b472991ea59152eada66095c61442aa
Riccardo-Gucciardo <riccardogucciardo003@gmail.com>
2025-02-12 21:52:43 +0100

    creazione app

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express()
+const port = 4000;
+
+
+
+
+
+
+
+
+app.listen(port, ()=>{
+    console.log(`example app listening on port ${port}`);
+    
+})
